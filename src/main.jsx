bf29d2bba97cea8aca9a91b5ad286258502d6400
Your Name <you@example.com>
2026-06-30 12:30:40 +0530

    feat: add Vercel Analytics tracking

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import './style.css'
 import CartProvider from "./context/CartContext";
 import App from './App.jsx'
 import AuthProvider from './context/AuthContext.jsx';
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
       <CartProvider>
         <App />
+        <Analytics />
       </CartProvider>
     </AuthProvider>
   </StrictMode>,
